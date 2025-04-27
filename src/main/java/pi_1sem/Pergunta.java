aacea5ab1e6d9947ab133d2d83a402364dd3b1ae
Guilherme Guerreiro <guilherme0606007@gmail.com>
2025-04-26 23:54:34 -0300

    first commit

---
+++ src/main/java/pi_1sem/Pergunta.java
@@ -0,0 +1,12 @@
+package pi_1sem;
+
+public class Pergunta {
+    private String enunciado;
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+}
