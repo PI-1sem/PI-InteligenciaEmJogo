4548ad510619091bb8362b065f6e61c1869fc0d9
Guilherme Guerreiro <guilherme0606007@gmail.com>
2025-05-01 22:26:21 -0300

    feat: add the conectionFactory to a db on cloud

---
+++ src/main/java/pi_1sem/conexao/ConectionFactory.java
@@ -0,0 +1,31 @@
+package pi_1sem.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConectionFactory {
+    
+    private String host= "pi-1-sem-pi1sem.c.aivencloud.com";
+    private String port= "24011";
+    private String db= "defaultdb";
+    private String user= "avnadmin";
+    private String password= "AVNS_86fkCYWmzq-dDh0o1iU";
+    
+    public Connection obterConexão() throws Exception{
+        var s= String.format("jdbc:mysql://%s:%s/%s", host, port, db);
+        Connection c= DriverManager.getConnection(s, user, password);
+        return c;
+    }
+    
+    public static void main(String[] args) throws Exception{
+        var fabricaDeConexoes= new ConectionFactory();
+        Connection conexao= fabricaDeConexoes.obterConexão();
+        if(conexao != null){
+            System.out.println("Conectou");
+        }
+        else{
+            System.out.println("Não conectou");
+        }
+    }
+}
+
