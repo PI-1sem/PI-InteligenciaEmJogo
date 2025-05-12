abd7cf4613a86d7c537b1206dbdb236935d9965c
Guilherme Guerreiro <guilherme0606007@gmail.com>
2025-05-11 21:13:01 -0300

    fix:connexionFactory is now working and safe

--- src/main/java/pi_1sem/conexao/ConectionFactory.java
+++ src/main/java/pi_1sem/conexao/ConectionFactory.java
@@ -8,8 +8,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ConectionFactory {
 
     Dotenv dotenv = Dotenv.load();
-    private String host= "pi-1-sem-pi1sem.c.aivencloud.com";
-    private String port= "24011";
+    private String host= "pi1sem-inteligenciaemjogopi-3060.l.aivencloud.com";
+    private String port= "28398";
     private String db= "defaultdb";
     private String user= "avnadmin";
     private String password= dotenv.get("passwordDb");
@@ -18,17 +18,6 @@ public class ConectionFactory {
         var s= String.format("jdbc:mysql://%s:%s/%s", host, port, db);
         Connection c= DriverManager.getConnection(s, user, password);
         return c;
-    }
-    
-    public static void main(String[] args) throws Exception{
-        var fabricaDeConexoes= new ConectionFactory();
-        Connection conexao= fabricaDeConexoes.obterConexão();
-        if(conexao != null){
-            System.out.println("Conectou");
-        }
-        else{
-            System.out.println("Não conectou");
-        }
     }
 }
 
